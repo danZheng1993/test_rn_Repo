8fae5f8c5690bd78efbb5f4ababff7da33ff13c4
chernandez7 <chris00hernandez@gmail.com>
2019-05-24 17:35:17 -0500

    add focused tab dot

--- src/components/TabBarIcon.js
+++ src/components/TabBarIcon.js
@@ -1,8 +1,31 @@
 import React from "react";
-import { Image } from "react-native";
+import { Image, View, StyleSheet } from "react-native";
 
-// TODO: Implement dot if focused
+const Style = StyleSheet.create({
+  container: {
+    width: "100%",
+    position: "relative",
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "center",
+    alignContent: "center",
+    alignItems: "center"
+  },
+  dot: {
+    position: "absolute",
+    bottom: -4,
+    width: 8,
+    height: 8,
+    borderRadius: 4,
+    backgroundColor: "#C2B48D"
+  }
+});
 
-const TabBarIcon = ({ icon, focused }) => <Image source={icon} />;
+const TabBarIcon = ({ icon, focused }) => (
+  <View style={Style.container}>
+    <Image source={icon} />
+    {focused && <View style={Style.dot} />}
+  </View>
+);
 
 export default TabBarIcon;
