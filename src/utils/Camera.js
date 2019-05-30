c6c32a670617e5a9ba26db8468a2693f9e28bee5
chernandez7 <chris00hernandez@gmail.com>
2019-05-29 23:05:03 -0500

    added camera and camera roll permissions

--- src/utils/Camera.js
+++ src/utils/Camera.js
@@ -1,8 +1,34 @@
-const GetCameraPermissions = () => {
-  // TODO: Implement
+import { Alert } from "react-native";
+import { Permissions, ImagePicker } from "expo";
+import { GetCameraRollPermissions } from "./";
+
+const GetCameraPermissions = async () => {
+  const { status } = await Permissions.askAsync(Permissions.CAMERA);
+  return status === "granted";
 };
-const GetCameraImage = () => {
-  // TODO: Implement
+
+const GetCameraImage = async () => {
+  const options = {
+    quality: 0.5,
+    base64: true,
+    mediaTypes: "Images",
+    allowsEditing: true,
+    exif: true,
+    aspect: [1, 1]
+  };
+  const permission = await GetCameraPermissions();
+  const permission2 = await GetCameraRollPermissions();
+  if (permission && permission2) {
+    try {
+      const image = await ImagePicker.launchCameraAsync(options);
+      return image;
+    } catch (err) {
+      return null;
+    }
+  } else {
+    Alert.alert("Camera access required.");
+    return null;
+  }
 };
 
 export { GetCameraPermissions, GetCameraImage };
