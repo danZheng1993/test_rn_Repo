59dd925d65d3d946e72f91dccb50e8e07877436a
chernandez7 <chris00hernandez@gmail.com>
2019-06-05 22:19:45 -0500

    structure styling for card modal

--- src/components/Modals/CardModal.js
+++ src/components/Modals/CardModal.js
@@ -10,14 +10,51 @@ const Style = StyleSheet.create({
   container: {
     height: "100%",
     width: "100%",
-    backgroundColor: "#2D2D2D"
+    backgroundColor: "#2D2D2D",
+    flexDirection: "column",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    alignContent: "center"
   },
-  headerContainer: {},
-  firstContainer: {},
-  buttonContainer: {},
-  spotifyContainer: {},
-  text: {},
-  avatar: {},
+  headerContainer: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    alignContent: "center"
+  },
+  firstContainer: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    alignContent: "center"
+  },
+  buttonContainer: {
+    width: "100%",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    alignContent: "center"
+  },
+  spotifyContainer: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    alignContent: "center"
+  },
+  text: {
+    fontFamily: "CircularProBook",
+    fontSize: 14,
+    lineHeight: 19,
+    color: "#FFFFFF"
+  },
+  avatar: {
+    width: 30,
+    height: 30,
+    borderRadius: 15
+  },
   spotify: {}
 });
 
